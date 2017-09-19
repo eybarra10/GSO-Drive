e4df9ffea4e08d526afd38dab3d3b9da5caf8678
Emilio Ybarra <eybarra10@outlook.com>
2017-09-19 13:41:27 +0200

    Removed Module

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -14,4 +14,10 @@ class HomeController @Inject() extends Controller {
   def userStartPage = Action {
     Ok(views.html.userStartPage.apply())
   }
+
+  /*def createProfilePost: Action[AnyContent] = action.async { implicit request =>
+    request.body.asFormUrlEncoded map { bodyMap =>
+    }
+  }*/
 }
+
